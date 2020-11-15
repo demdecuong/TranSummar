softmax bug

transformer.py
@@ -122,14 +122,8 @@ class MultiheadAttention(nn.Module):
 
         attn_weights = torch.bmm(q, k.transpose(1, 2))
         context_vectors = torch.bmm(k.transpose(1,2) , v)
-        context_vectors = F.softmax(q,context_vectors.transpose(1,2))
-
-        if with_external:
-            attn_context = torch.bmm(F.softmax(k,dim = -1), qvi_v.transpose(1, 2))
-            attn_weights = torch.bmm(attn_weights,attn_context)
-
-        if self.is_encoder:
-            qvi = torch.mul(F.softmax(q,dim=-1), qvi_v) + v
+        context_vectors = torch.bmm(q,context_vectors.transpose(1,2))
+        context_vectors = F.softmax(context_vectors,dim = -1)
         
         assert list(attn_weights.size()) == [bsz * self.num_heads, tgt_len, src_len]
         